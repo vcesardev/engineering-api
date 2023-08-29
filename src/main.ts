6b3714a48c9c8f38f77a40c0daf8454cfbbaf298
Vitor Cesar <vitor@fasters.com.br>
2023-08-28 21:33:45 -0300

    fix: attempt deploy 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
