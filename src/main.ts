27a91651c250629eb070c50fac9d62bc0b73c2f5
Vitor Cesar <vitor@fasters.com.br>
2023-08-31 14:56:26 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://ec2-3-131-153-153.us-east-2.compute.amazonaws.com:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(Number(process.env.PORT), '0.0.0.0', () => {
-    console.log('app listening port');
+    console.log('app listening port', process.env.PORT);
   });
 }
 bootstrap();
