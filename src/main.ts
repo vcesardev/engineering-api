e3bcb13cb45c9d42ce36f7b3d6cc63dc72d6eaef
Vitor Cesar <vitor@fasters.com.br>
2023-08-31 15:39:16 -0300

    feat: attempt to enable all cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://ec2-3-131-153-153.us-east-2.compute.amazonaws.com:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
